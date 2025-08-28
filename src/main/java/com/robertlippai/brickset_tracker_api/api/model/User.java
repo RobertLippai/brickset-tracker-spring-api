611ea97af71394b4a7f7bd1216cb2a991a090f0a
RobertLippai <robertlippai25@gmail.com>
2025-08-29 00:44:53 +0200

    Set up initial Spring Security user configuration

--- src/main/java/com/robertlippai/brickset_tracker_api/api/model/User.java
+++ src/main/java/com/robertlippai/brickset_tracker_api/api/model/User.java
@@ -2,7 +2,13 @@ package com.robertlippai.brickset_tracker_api.api.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 import java.util.HashSet;
 
@@ -12,7 +18,7 @@ import java.util.HashSet;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,6 +30,9 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @ColumnDefault("'user'")
+    private String role;
+
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "user_sets",
@@ -31,4 +40,9 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "sid")
     )
     private Set<BrickSet> ownedSets = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
 }
