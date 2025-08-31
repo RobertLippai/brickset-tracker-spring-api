e1cde15704ff05da025eccf9b9260c2ee0bcbe7d
RobertLippai <robertlippai25@gmail.com>
2025-08-31 22:00:24 +0200

    Added JWT dependencies and authentication DTOs

---
+++ src/main/java/com/robertlippai/brickset_tracker_api/api/dto/UserLoginRequestDto.java
@@ -0,0 +1,6 @@
+package com.robertlippai.brickset_tracker_api.api.dto;
+
+public class UserLoginRequestDto {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/robertlippai/brickset_tracker_api/api/dto/UserRegistrationRequestDto.java
@@ -0,0 +1,12 @@
+package com.robertlippai.brickset_tracker_api.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegistrationRequestDto {
+    private String fullName;
+    private String username;
+    private String password;
+}
